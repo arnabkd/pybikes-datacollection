24a03992f0090fa16fd87ea4322fe0162ae9085c
Arnab Datta <arnabkd@student.uio.no>
2014-04-10 00:19:54 +0200

    added sanitizer function for racks in Oslo

--- src/transform-script/transformer2.py
+++ src/transform-script/transformer2.py
@@ -1,4 +1,4 @@
-import sys, os, glob, datetime
+import sys, os, glob, datetime, json
 from collections import namedtuple
 
 """
@@ -23,7 +23,9 @@ def get_timestamp(ts):
   st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
   return st
 
-
+"""
+  Transform old content into new-style data
+"""
 def transform(old_content, target_dir, racks):
   #Each line should create a new file
   for line in old_content:
@@ -63,6 +65,7 @@ def transform(old_content, target_dir, racks):
    Get rack data from a file with a list of namedtuples (in this format)
 """
 def get_rack_data(rack_data_file):
+  invalid_racks = []
   racks_info = get_file_lines(rack_data_file)
   Rack = namedtuple("Rack", ['number','description','latitude', 'longitude', 'online', 'bikes', 'locks'])
   racks = {}
@@ -75,13 +78,37 @@ def get_rack_data(rack_data_file):
     rack_dict ['latitide'] = rack.latitude
     rack_dict ['longitude'] = rack.longitude
     rack_dict ['capacity'] = rack.bikes + rack.locks
-
+    
+    #if rack_dict ['capacity'] < 1:
+    invalid_racks.append(rack.number)
     racks[rack.number] = rack_dict
 
-  return racks
+  return invalid_racks, racks
 
-
-
+"""
+   Sanitize the capacities of the invalid racks, given a list of files to extract maximums from.
+   Write a racks-dict.json to the src_dir
+"""
+def sanitize_rack_capacities(src_dir, files_list, racks, invalid_racks):
+  #print files_list
+  print "Invalid capacities for racks: ", invalid_racks
+  print "Trying to find proper value in %s files"%(len(files_list))
+  Rack = namedtuple("Rack", ['number','description','latitude', 'longitude', 'online', 'bikes', 'locks'])
+  
+  for f in files_list:
+    content = get_file_lines(f)
+    
+    for line in content:
+      status = eval(line)[1]
+      for rack in invalid_racks:
+        #If the new capacity exceeds what is previously recorded
+        if rack in status.keys() and status[rack] > racks[rack]['capacity']:
+          #print "updating capacity for rack %s to %s"%(rack, status[rack])
+          racks[rack]['capacity'] = status[rack]
+  json_content = json.dumps(racks, indent=4, sort_keys=True)
+  fo = open(src_dir + "racks_dict.json", "w")
+  fo.write(json_content)
+  fo.close()
 
 ##############################################################################################################
 ################## Transform script ##########################################################################
@@ -89,7 +116,7 @@ def get_rack_data(rack_data_file):
 
 #Scan for files in this directory
 src_dir = "../../2013-2014-Oslo/bikes2/"
-files = glob.glob(src_dir + "2013*")
+files = glob.glob(src_dir + "20*")
 #print "%s files found in %s, that contain oslobysykkel data"%(len(files), src_dir)
 
 #Create a data directory if one does not exist  
@@ -100,8 +127,15 @@ if not os.path.exists(target_dir):
   os.mkdir(target_dir)
   print "Directory created successfully" if  os.path.exists(target_dir) else "Could not create directory"
 
-racks = get_rack_data(src_dir + "racks.txt")
 
+invalid_racks, racks = get_rack_data(src_dir + "racks.txt")
+if len(invalid_racks) > 0:
+  sanitize_rack_capacities(src_dir, files, racks, invalid_racks)
+
+for id in racks.keys():
+  print racks[id]
+
+"""
 i = 0
 for old_style_file in files:
   old_content = get_file_lines(old_style_file)
@@ -109,3 +143,4 @@ for old_style_file in files:
   i += 1
   print "transformed %s files"%(i)
   
+"""
