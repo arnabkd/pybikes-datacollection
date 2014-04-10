d1ccf314838e8c2c7575a7093b293b9128657a4b
Arnab Datta <arnabkd@student.uio.no>
2014-04-10 23:10:39 +0200

    Modify transform script to use eval with a racks-dict file instead

--- src/transform-script/transformer2.py
+++ src/transform-script/transformer2.py
@@ -23,6 +23,17 @@ def get_timestamp(ts):
   st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
   return st
 
+def missing_elements(L, subL):
+  L2 = list(L)
+  subL2 = list(subL)
+  print "recieved a list with ", len(L2), "elements"
+  print "received a sublist with", len(subL), "elements"
+  for elem in subL2:
+    if str(elem) in L2:
+      L2.remove(elem)
+  print "returning a list with", len(L2), "elements"
+  return L2
+
 """
   Transform old content into new-style data
 """
@@ -32,7 +43,6 @@ def transform(old_content, target_dir, racks):
     res = eval(line)
     filename = target_dir + get_timestamp(res[0]) + ".json"
     #print "epoch", res[0], "=> FileName: ", (target_dir+filename)
-    #print filename
    
     #Create a new file, transform the content and write to it
     status = res[1]
@@ -44,15 +54,12 @@ def transform(old_content, target_dir, racks):
     
     #Transform content
     json_content = "{ \"city\": %s, \"time\": %s\n, \"stations\": [\n"%("\"Oslo\"", res[0])
-    size = len(status)
-    
-    for key in status.keys():
-      if key in racks.keys():
-        bikes = status[key]
-        free = racks[key]['capacity'] - bikes
-        json_content += "{\"id\": %s, \"bikes\": %s, \"free\": %s},\n"%(key, bikes,free)
         
-      
+    for id in status.keys():
+      bikes = status[id]
+      if id in racks.keys():
+        free = racks[id]['capacity'] - bikes
+        json_content += "{\"id\": %s, \"bikes\": %s, \"free\": %s},\n"%(id, bikes,free)
     json_content = json_content[:-2]
     json_content += "]}\n"
 
@@ -81,7 +88,7 @@ def get_rack_data(rack_data_file):
     
     #if rack_dict ['capacity'] < 1:
     invalid_racks.append(rack.number)
-    racks[rack.number] = rack_dict
+    racks[(int)(rack.number)] = rack_dict
 
   return invalid_racks, racks
 
@@ -90,7 +97,6 @@ def get_rack_data(rack_data_file):
    Write a racks-dict.json to the src_dir
 """
 def sanitize_rack_capacities(src_dir, files_list, racks, invalid_racks):
-  #print files_list
   print "Invalid capacities for racks: ", invalid_racks
   print "Trying to find proper value in %s files"%(len(files_list))
   Rack = namedtuple("Rack", ['number','description','latitude', 'longitude', 'online', 'bikes', 'locks'])
@@ -105,9 +111,12 @@ def sanitize_rack_capacities(src_dir, files_list, racks, invalid_racks):
         if rack in status.keys() and status[rack] > racks[rack]['capacity']:
           #print "updating capacity for rack %s to %s"%(rack, status[rack])
           racks[rack]['capacity'] = status[rack]
-  json_content = json.dumps(racks, indent=4, sort_keys=True)
-  fo = open(src_dir + "racks_dict.json", "w")
-  fo.write(json_content)
+  #json_content = json.dumps(racks, indent=4, sort_keys=True)
+  #fo = open(src_dir + "racks_dict.json", "w")
+  #fo.write(json_content)
+  #fo.close()
+  fo = open(src_dir + "racks_dict", "w")
+  fo.write(str(racks))
   fo.close()
 
 
@@ -118,7 +127,7 @@ def sanitize_rack_capacities(src_dir, files_list, racks, invalid_racks):
 ##############################################################################################################
 
 #Scan for files in this directory
-src_dir = "../../2013-2014-Oslo/bikes2/"
+src_dir = "../../2013-2014-Oslo/bikes3/"
 files = glob.glob(src_dir + "20*")
 #print "%s files found in %s, that contain oslobysykkel data"%(len(files), src_dir)
 
@@ -134,22 +143,23 @@ racks = {}
 processed_files = {}
 
 #If there is no racks-dict.json file, create one from the racks.txt file, and sanitize all stations
-if not os.path.exists(src_dir+"racks_dict.json"):
-  print "There is no racks-dict.json file, create one from the racks.txt file, and sanitize all stations"
+if not os.path.exists(src_dir+"racks_dict"):
+  print "There is no racks-dict file, create one from the racks.txt file, and sanitize all stations"
   invalid_racks, racks = get_rack_data(src_dir + "racks.txt")
   if len(invalid_racks) > 0:
     sanitize_rack_capacities(src_dir, files, racks, invalid_racks)
 else:
-  print "Racks information file found"
-  content = get_file_content(src_dir + "racks_dict.json")
-  racks = json.loads(content)
-  
+  print "Racks information file found. Using eval to get racks-dict"
+  content = get_file_content(src_dir + "racks_dict")
+  racks = eval(content)
+
+
+
 processed_files_jsonfile = src_dir + "processed_files.json"
 if not os.path.exists(processed_files_jsonfile):
   processed_files = {"done": []}
 else:
   processed_files = json.loads(get_file_content(processed_files_jsonfile))
-
 i = 0
 for old_style_file in files:
   if old_style_file not in processed_files['done']:
